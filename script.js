f494a9ab18d99500dde4b288e23954c1a8dffc82
Melnichuk2 <105264214+Melnichuk2@users.noreply.github.com>
2024-02-06 16:56:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 function toggleLocations() {
-    var locationsList = document.getElementById('locations-list');
+    var locationsToggle = document.getElementById('park-toggle','academy-toggle','university-toggle');
     if (locationsList.style.display === "none") {
         locationsList.style.display = "block";
     } else {
         locationsList.style.display = "none";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var parkToggle = document.getElementById('park-toggle');
+    var universityToggle = document.getElementById('university-toggle');
+    var academyToggle = document.getElementById('academy-toggle');
+    var locationsList = document.getElementById('location-list');
+
+    parkToggle.addEventListener('mouseenter', function() {
+        locationsList.classList.remove('hidden');
+    });
+
+    universityToggle.addEventListener('mouseenter', function() {
+        locationsList.classList.remove('hidden');
+    });
+
+    academyToggle.addEventListener('mouseenter', function() {
+        locationsList.classList.remove('hidden');
+    });
+
+    locationsList.addEventListener('mouseleave', function() {
+        locationsList.classList.add('hidden');
+    });
+});
+
 
 function showLocation(location) {
     var locationImage = document.getElementById('location-image');
@@ -15,10 +39,6 @@ function showLocation(location) {
         case 'Головних вхід на територію університету':
             locationImage.src = "images/entrance.jpg";
             locationDescription.innerText = "Головний вхід на територію університету - це місце, через яке ви входите на територію університету.";
-            break;
-        case 'Парк':
-            locationImage.src = "парк 1.jpg";
-            locationDescription.innerText = "Парк на території університету - це мальовнича територія з деревами, квітами та альтанками, де можна відпочити та насолодитися природою.";
             break;
         case 'Басейн':
             locationImage.src = "басейн.jpg";
@@ -32,12 +52,21 @@ function showLocation(location) {
             locationImage.src = "ліцей.jpg";
             locationDescription.innerText = "Херсонський академічний ліцей ім.О.В.Мішукова";
             break;
-        case 'Головна площа':
-            locationImage.src = "центр.jpg";
-            locationDescription.innerText = "Головна площа університету - це центральне місце на території університету, де проводяться різні заходи та заходи.";
+        case 'Фонтан':
+            locationImage.src = "ліцей.jpg";
+            locationDescription.innerText = "Фонтан";
             break;
-        
+        case 'Бібліотека':
+            locationImage.src = "ліцей.jpg";
+            locationDescription.innerText = "Бібліотека";
+            break;
+        case 'Розягальня':
+            locationImage.src = "ліцей.jpg";
+            locationDescription.innerText = "Роздягальня";
+            break;    
+       
     }
+
 }
 
 function goToHome() {
@@ -45,5 +74,6 @@ function goToHome() {
 }
 
 function goToSocialMedia() {
+    
     window.location.href = "https://www.kspu.edu/";
 }
