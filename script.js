2c46d22f507d06acd346c52adddfff79598095c3
Melnichuk2 <105264214+Melnichuk2@users.noreply.github.com>
2024-02-06 17:11:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,26 +8,17 @@ function toggleLocations() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var parkToggle = document.getElementById('park-toggle');
-    var universityToggle = document.getElementById('university-toggle');
-    var academyToggle = document.getElementById('academy-toggle');
+    var locationsToggle = document.getElementById('locations-toggle');
     var locationsList = document.getElementById('location-list');
 
-    parkToggle.addEventListener('mouseenter', function() {
-        locationsList.classList.remove('hidden');
-    });
-
-    universityToggle.addEventListener('mouseenter', function() {
-        locationsList.classList.remove('hidden');
-    });
-
-    academyToggle.addEventListener('mouseenter', function() {
+    locationsToggle.addEventListener('mouseenter', function() {
         locationsList.classList.remove('hidden');
     });
 
     locationsList.addEventListener('mouseleave', function() {
         locationsList.classList.add('hidden');
     });
+
 });
 
 
@@ -36,32 +27,24 @@ function showLocation(location) {
     var locationDescription = document.getElementById('location-description');
     
     switch(location) {
-        case 'Головних вхід на територію університету':
-            locationImage.src = "images/entrance.jpg";
-            locationDescription.innerText = "Головний вхід на територію університету - це місце, через яке ви входите на територію університету.";
-            break;
         case 'Басейн':
             locationImage.src = "басейн.jpg";
             locationDescription.innerText = "Басейн на території університету - це спортивний комплекс з басейном для плавання та різноманітних водних розваг.";
             break;
-        case 'Міні футбольний стадіон':
-            locationImage.src = "images/football_stadium.jpg";
-            locationDescription.innerText = "Міні футбольний стадіон - це спортивний майданчик для ігор у футбол на невеликому масштабі.";
-            break;
-        case 'Вхід до Херсонського академічног ліцею ім.О.В.Мішукова':
+        case 'Ліцей':
             locationImage.src = "ліцей.jpg";
             locationDescription.innerText = "Херсонський академічний ліцей ім.О.В.Мішукова";
             break;
         case 'Фонтан':
-            locationImage.src = "ліцей.jpg";
+            locationImage.src = "дддд.jpg";
             locationDescription.innerText = "Фонтан";
             break;
         case 'Бібліотека':
-            locationImage.src = "ліцей.jpg";
+            locationImage.src = "шшшш.jpg";
             locationDescription.innerText = "Бібліотека";
             break;
         case 'Розягальня':
-            locationImage.src = "ліцей.jpg";
+            locationImage.src = "ооо.jpg";
             locationDescription.innerText = "Роздягальня";
             break;    
        
