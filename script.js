23787a17384532fc96fdbac6b20a529b536cb287
Melnichuk2 <105264214+Melnichuk2@users.noreply.github.com>
2024-02-07 12:39:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
-function toggleLocations() {
-    var locationsToggle = document.getElementById('park-toggle','academy-toggle','university-toggle');
-    if (locationsList.style.display === "none") {
-        locationsList.style.display = "block";
-    } else {
-        locationsList.style.display = "none";
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var locationsToggle = document.getElementById('locations-toggle');
-    var locationsList = document.getElementById('location-list');
-
-    locationsToggle.addEventListener('mouseenter', function() {
-        locationsList.classList.remove('hidden');
-    });
-
-    locationsList.addEventListener('mouseleave', function() {
-        locationsList.classList.add('hidden');
-    });
-
-});
-
 
 function showLocation(location) {
     var locationImage = document.getElementById('location-image');
     var locationDescription = document.getElementById('location-description');
     
     switch(location) {
-        case 'Басейн':
-            locationImage.src = "басейн.jpg";
-            locationDescription.innerText = "Басейн на території університету - це спортивний комплекс з басейном для плавання та різноманітних водних розваг.";
-            break;
-        case 'Ліцей':
-            locationImage.src = "ліцей.jpg";
-            locationDescription.innerText = "Херсонський академічний ліцей ім.О.В.Мішукова";
+        case 'Парк':
+            locationImage.src = "jpg/парк 1.jpg";
+            locationDescription.innerText = "Парк на території університету - це мальовнича територія з деревами, квітами та альтанками, де можна відпочити та насолодитися природою.";
             break;
         case 'Фонтан':
-            locationImage.src = "дддд.jpg";
-            locationDescription.innerText = "Фонтан";
+            locationImage.src = "jpg/фонтан.jpg";
+            locationDescription.innerText = "Басейн на території університету - це спортивний комплекс з басейном для плавання та різноманітних водних розваг.";
+            break;     
+        case 'Університет':
+            locationImage.src = "images/football_stadium.jpg";
+            locationDescription.innerText = "Херсонський Державний Університет - найбільш відомий інститут Херсонської області. Один з найстаріших ВНЗ на півдні в Україні. Готує спеціалістів з 51 спеціальностей. Це вчителі з усіх предметів, що вивчаються в школі, вихователі дитячих закладів, інженери-педагоги, юристи, економісти, екологи, психологи, наукові співробітники, соціальні працівники, журналісти, перекладачі та ін. Професорсько-викладацький колектив складається з професорів, 58 докторів наук, 333 доцентів, кандидатів наук, 105 старших викладачів, асистентів."
+            "Серед викладачів університету є почесні академіки українських і міжнародних галузевих академій, майже 40 вчених мають почесні звання у сферах професійної діяльності, зокрема народні художники й артисти, заслужені художники та тренери України, заслужені працівники в галузях освіти, науки і техники, економіки, юриспруденції, культури тощо. ";
             break;
-        case 'Бібліотека':
-            locationImage.src = "шшшш.jpg";
-            locationDescription.innerText = "Бібліотека";
+        case 'Вхід до ліцею':
+            locationImage.src = "jpg/ліцей.jpg";
+            locationDescription.innerText = "Вхід до Херсонського академічного ліцею ім.О.В.Мішукова";
             break;
-        case 'Розягальня':
-            locationImage.src = "ооо.jpg";
-            locationDescription.innerText = "Роздягальня";
-            break;    
+        case 'Центральна площа':
+            locationImage.src = "jpg/центр.jpg";
+            locationDescription.innerText = "Головна площа університету - це центральне місце на території університету, де проводяться різні заходи та заходи.";
+            break;
+        case 'Спортивний майданчик':
+            locationImage.src = "jpg/спорт.jpg";
+            locationDescription.innerText = "Спортивний майданчик для молоді ";
+            break;   
+        case 'Вхід до':
+            locationImage.src = "jpg/вхідхду.jpg";
+            locationDescription.innerText = "Головний вхід до уорпусу університету ";
+            break;      
+         
        
     }
-
 }
 
 function goToHome() {
